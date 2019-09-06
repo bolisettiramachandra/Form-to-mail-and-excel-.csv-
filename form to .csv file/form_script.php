cd036685bf268d2859b88254f08760edeafada69
rama chandra <40751706+bolisettiramachandra@users.noreply.github.com>
2019-09-06 08:40:00 +0530

    Add files via upload

---
+++ form to .csv file/form_script.php
@@ -0,0 +1,28 @@
+<?php
+
+$email=$_POST['email'];
+$data=$email;
+
+
+$file="nosmar.csv";
+
+
+file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
+
+?>
+<html>
+<head>
+    <title>ThankYou Page</title>    
+</head>
+<body>
+    <br><br><br><br><br><br><br>
+    <h1 align=center>Thank you ..!</h1>
+    <center>
+<h4><a href="form.html">HOME</a> </h4> </center>   
+    
+</body>
+
+
+
+
+</html>
